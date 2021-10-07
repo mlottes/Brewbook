f30bbecf93a354af0ef52b6260835257260f458d
mlottes <mlottes1@gmail.com>
2021-10-07 18:53:02 -0400

    First commit. Add skeleton project code to github repo.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.brewbook.brewbook {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.brewbook.brewbook to javafx.fxml;
+    exports com.brewbook.brewbook;
+}
